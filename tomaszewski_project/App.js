a56280d90ee7d2194ac2a71489b43b544700a775
Krzysztof Tomaszewski <62775389+KrzysztofDTomaszewski@users.noreply.github.com>
2022-03-24 19:50:57 +0000

    Updated Login Form
    Added more user variables

--- tomaszewski_project/App.js
+++ tomaszewski_project/App.js
@@ -1,5 +1,8 @@
+// React Detrimental Class Component Imports
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+
+//Import Screens
 import Login from './components/Login';
 
 class App extends Component {
