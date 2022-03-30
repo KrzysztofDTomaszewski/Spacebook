d699f088cc537695ee776cc26c8057e345122443
Krzysztof Tomaszewski <62775389+KrzysztofDTomaszewski@users.noreply.github.com>
2022-03-30 21:13:18 +0100

    Re-instated Node
    Modules became corrupt and screens refused to work with stack navigator

---
+++ Tomaszewski_Project/App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Login from 'components/screens';
+export default function App() {
+  return (
+    <NavigationContainer><Login /></NavigationContainer>
+  );
+}
